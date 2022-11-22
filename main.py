ca78c6974ed3f6b6414bdd639f2d814b2a41c8f9
James Waterworth <james@yumpingo.com>
2022-11-22 20:46:33 +0000

    sleep 10 seconds

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ while True:
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
-        time.sleep(2.0)
+        time.sleep(20.0)
         continue
     except Exception as error:
         dhtDevice.exit()
