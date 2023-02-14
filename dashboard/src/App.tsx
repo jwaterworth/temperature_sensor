ca7f65a7d8f220873470daa095a4a3826219425e
James Waterworth <james@yumpingo.com>
2023-02-14 20:18:07 +0000

    add dashboard template

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { TemperatureChart } from "./components/temperature-chart";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <TemperatureChart />
-      </header>
+    <div>
+      <Dashboard />
     </div>
   );
 }
