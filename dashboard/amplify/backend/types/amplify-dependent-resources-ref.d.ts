c2c58e27a8d4b46f37b88f5ce6654979a220a944
James Waterworth <james@yumpingo.com>
2023-07-04 19:42:24 +0100

    fix prod env for dashboard via amplify

--- dashboard/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ dashboard/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,10 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "hosting": {
-    "S3AndCloudFront": {
-      "HostingBucketName": "string",
-      "Region": "string",
-      "S3BucketSecureURL": "string",
-      "WebsiteURL": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
